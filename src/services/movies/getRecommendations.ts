56945ca49c271385c8927ba6dfe8ec03e1a6dfee
EstherRC <a01656060@tec.mx>
2024-05-05 01:26:23 -0600

    Security modified

--- src/services/movies/getRecommendations.ts
+++ src/services/movies/getRecommendations.ts
@@ -1,11 +1,11 @@
 import httpInstance from "../httpInstance";
 
 export const getRecommendations = async (movieId: String) => {
-    const endpoint = `${movieId}/recommendations?api_key=${process.env.REACT_APP_MDB_API_KEY}&language=en-US`;
+    const endpoint = `${movieId}/recommendations?api_key=${process.env.REACT_APP_MDB_API}&language=en-US`;
     try {
         const response = await httpInstance.get(endpoint);
         if (response.data && response.data.results) {
-            return response.data.results; // Asegúrate de que estamos accediendo correctamente
+            return response.data.results; 
         } else {
             console.log("Response is missing 'data' or 'results'");
             return [];
